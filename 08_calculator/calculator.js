c144a7842ebc856699c650b9745af571a9442d9b
Ann2909 <149702668+Ann2909@users.noreply.github.com.>
2024-07-18 03:14:20 +0700

    Finish number 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, current) => accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1;
+  for (let i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  } else {
+    let fac = 1;
+    for (let i = 0; i < a; i++) {
+      fac *= a - i;
+    }
+    return fac;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let allPunctuations = [',' , '.' , '?' , '!' , ';' , ':', ' ']; //a better version would be to just list out what we want like all the letters in the alphabet and all numbers possible, then filter everything else
+    let newStr = '';
+    let compareStr = str.split('').filter((item) => allPunctuations.includes(item)).join(''); //no more punctuations and spaces
 
+    
+    newStr = compareStr.split('').reverse().join(''); 
+    return (newStr.toLowerCase() === compareStr.toLowerCase()) //so that it works with all lower or upper cases
 };
 
 // Do not edit below this line
